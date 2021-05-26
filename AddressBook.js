bff7a5c0a5f4d20ffa083ac80db42de94e2afd84
Anikesh <anikesh@0725@gmail.com>
2021-05-26 20:37:48 +0530

    Addition : Adding regex and Exception Handeling

--- AddressBook.js
+++ AddressBook.js
@@ -1,14 +1,46 @@
+
+
+let firstNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+let lastNameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+let addressRegex = RegExp("^.{4,}$");
+let cityRegex = RegExp("^[a-zA-Z]{4,}$");
+let stateRegex = RegExp("^[a-zA-Z\\s]{4,}$");
+let zipCodeRegex = RegExp("^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$");
+let phoneRegex = RegExp("^[0-9]{1,}$");
+let emailRegex = RegExp("^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$");
+
+class ValidationError extends Error {
+    constructor(message) {
+      super(message); 
+      this.name = "ValidationError"; 
+    }
+}
 class AddressBook{
     constructor(...params) {
         
-        this.firstName =params[0];
-        this.lastName =params[1];
-        this.address =params[2]; 
-        this.city =params[3];
-        this.state =params[4];
-        this.zip =params[5];
-        this.phNo =params[6];
-        this.email =params[7];
+        if(firstNameRegex.test(params[0])) this.firstName = params[0];
+        else throw new ValidationError("Invalid First Name: "+params[0]);
+
+        if(lastNameRegex.test(params[1])) this.lastName = params[1];
+        else throw new ValidationError("Invalid Last Name: "+params[1]);
+
+        if(addressRegex.test(params[2])) this.address = params[2];
+        else throw new ValidationError("Invalid Address: "+params[2]);
+        
+        if(cityRegex.test(params[3])) this.city = params[3];
+        else throw new ValidationError("Invalid City: "+params[3]);
+       
+        if(stateRegex.test(params[4])) this.state = params[4];
+        else throw new ValidationError("Invalid State: "+params[4]);
+        
+        if(zipCodeRegex.test(params[5])) this.zip= params[5];
+        else throw new ValidationError("Invalid ZIP Code: "+params[5]);
+    
+        if(phoneRegex.test(params[6])) this.phNo = params[6];
+        else throw new ValidationError("Invalid Phone Number: "+params[6]);
+
+        if(emailRegex.test(params[7])) this.email = params[7];
+        else throw new ValidationError("Invalid Email: "+params[7]);
     }
 
         getFirstName()
@@ -17,6 +49,7 @@ class AddressBook{
         }
         setFirstName(firstName)
         {
+            
             return this.firstName = firstName;
         }
         
@@ -92,6 +125,6 @@ class AddressBook{
         " "+ "Email :"+" "+ this.email;
     }
 }
-     let addressbook = new AddressBook("Anikesh","Mahajan","Akhnoor","Jammu","J&K","181201","1111111111","anikesh@0725@gmail.com");
+     let addressbook = new AddressBook("Anikesh","Mahajan","Akhnoor","Jammu","Jammu","181201","1111111111","anikesh0725@gmail.com");
      console.log(addressbook.toString());
 
